1e2ca23e9d810f3ced0edf1e19d48c63eb5f2918
Augusto Cesar <acs-03@hotmail.com>
2022-01-14 13:45:21 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 const Listing = () => {
+
+  axios.get(`${BASE_URL}/movies`)
+  .then(response => {
+    console.log(response.data);
+  })
+
   return (
     <>
       <Pagination />
